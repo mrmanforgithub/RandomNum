b321506978a344f48a18ade541d46610984d4a41
mrmanforgithub <3141356420@qq.com>
2024-07-07 20:48:43 +0800

    创建了一个随机数生成小工具

---
+++ control.py
@@ -0,0 +1,118 @@
+import random
+import time
+from tkinter import filedialog
+from openpyxl import load_workbook
+
+class Controller:
+    ui: object
+    def __init__(self):
+        self.load_if = None
+        pass
+    def init(self, ui):
+        self.ui = ui
+        # TODO 组件初始化 赋值操作
+    def save_txt(self):
+    # 弹出文件保存对话框
+        file_path = filedialog.asksaveasfilename(defaultextension='.txt',
+                                                filetypes=[('Text Files', '*.txt')],
+                                                title='保存为TXT文件')
+        # 如果用户取消了保存，则直接返回
+        if not file_path:
+            return
+        try:
+            # 打开选定的文件路径，以写入模式保存数据
+            with open(file_path, 'w', encoding='utf-8') as f:
+                # 获取表格数据，这里假设表格数据是字符串格式
+                for row in self.ui.tk_table_num_collect.get_children():
+                    row_data = self.ui.tk_table_num_collect.item(row)['values']
+                # 将每行数据转换为字符串并写入文件
+                    row_str = '\t'.join(str(item) for item in row_data) + '\n'
+                    f.write(row_str)
+        except Exception as e:
+            return
+
+
+    def create_number(self, start_num, end_num, generation_num, repeat_if, method, clear_if):
+        if start_num > end_num:
+            j = start_num
+            start_num = end_num
+            end_num = j
+        # start_num 是随机数最小值, end_num 是最大值, generation_num 是生成随机数个数, repeat_if 是是否生成重复随机数
+        # method是扫描方法
+        if method != "数字" and self.load_if:
+            wb = load_workbook(self.load_if)
+            sheet = wb.active
+            columns = int(method[:-1])
+            names_from_excel = [sheet.cell(row=i, column=columns).value for i in range(1, end_num + 1)if sheet.cell(row=i, column=columns).value is not None]
+            wb.close()
+            generated_numbers = self.generate_numbers(start_num, end_num, generation_num, repeat_if, load_num = len(names_from_excel))
+            if clear_if == "自动清除":
+                self.ui.tk_table_num_collect.delete(*self.ui.tk_table_num_collect.get_children())
+            for i, num in enumerate(generated_numbers, start=1):
+                name = names_from_excel[num - 1]
+                self.ui.tk_label_now_num.config(text=num)
+                self.ui.update()
+                time.sleep(0.02)
+                self.ui.tk_table_num_collect.insert('', 'end', values=(i, name))
+        else:
+            generated_numbers = self.generate_numbers(start_num, end_num, generation_num, repeat_if)
+            if clear_if == "自动清除":
+                self.ui.tk_table_num_collect.delete(*self.ui.tk_table_num_collect.get_children())
+            for i, num in enumerate(generated_numbers, start=1):
+                self.ui.tk_label_now_num.config(text=num)
+                self.ui.update()
+                time.sleep(0.02)
+                self.ui.tk_table_num_collect.insert('', 'end', values=(i, num))
+
+    def generate_numbers(self, start_num, end_num, generation_num, repeat_if, load_num = None):
+        if load_num is not None:
+            if load_num < start_num:
+                self.ui.tk_label_now_num.config(text="err")
+                return []
+            if repeat_if == "重复":
+                if load_num < end_num:
+                    end_num = load_num
+                generated_numbers = [random.randint(start_num, end_num) for _ in range(generation_num)]
+            elif repeat_if == "不重复":
+                if load_num < generation_num:
+                    generation_num = load_num
+                if (end_num - start_num + 1) < generation_num:
+                    generation_num = (end_num - start_num + 1)
+                if load_num < end_num:
+                    end_num = load_num
+                generated_numbers = random.sample(range(start_num, end_num + 1), generation_num)
+            return generated_numbers
+        else:
+            if repeat_if == "重复":
+                generated_numbers = [random.randint(start_num, end_num) for _ in range(generation_num)]
+            elif repeat_if == "不重复":
+                if (end_num - start_num + 1) < generation_num:
+                    generation_num = (end_num - start_num + 1)
+                generated_numbers = random.sample(range(start_num, end_num + 1), generation_num)
+            return generated_numbers
+    
+
+    def clear_table(self,evt):
+        self.ui.tk_table_num_collect.delete(*self.ui.tk_table_num_collect.get_children())
+        self.load_if = None
+        self.ui.tk_label_load_label.config(text="目前读取内容：空")
+        cb = self.ui.tk_select_box_select_method
+        cb['values'] =  ["数字"]  # 设置Combobox的选项为列数的范围
+        cb.set("数字")  # 默认选择第一列
+
+    def load_execl(self,evt):
+        file_path = filedialog.askopenfilename(filetypes=[("Excel files", "*.xlsx;*.xls")])
+        if file_path:
+            self.load_if = file_path
+            self.ui.tk_label_load_label.config(text=file_path)
+            wb = load_workbook(file_path)
+            sheet = wb.active  # 获取活动工作表，你也可以根据需要选择特定的工作表
+            # 获取列数
+            column_count = sheet.max_column
+            cb = self.ui.tk_select_box_select_method # 调用创建Combobox的方法
+            column_options = ["数字"] + [f"{i}列" for i in range(1, column_count + 1)]
+            cb['values'] = column_options  # 设置Combobox的选项为列数的范围
+            cb.set("1列")  # 默认选择第一列
+        else:
+            return None
+
---
+++ ui.py
@@ -0,0 +1,194 @@
+import random
+from tkinter import *
+from tkinter.ttk import *
+from ttkbootstrap import *
+from pytkUI.widgets import *
+class WinGUI(Window):
+    def __init__(self):
+        super().__init__(themename="cosmo", hdpi=False)
+        self.__win()
+        self.tk_input_start_num = self.__tk_input_start_num(self)
+        self.tk_label_from_label = self.__tk_label_from_label(self)
+        self.tk_input_end_num = self.__tk_input_end_num(self)
+        self.tk_label_now_num = self.__tk_label_now_num(self)
+        self.tk_button_create_num = self.__tk_button_create_num(self)
+        self.tk_table_num_collect = self.__tk_table_num_collect(self)
+        self.tk_label_start_label = self.__tk_label_start_label(self)
+        self.tk_label_end_label = self.__tk_label_end_label(self)
+        self.tk_input_generation_num = self.__tk_input_generation_num(self)
+        self.tk_label_generation_label = self.__tk_label_generation_label(self)
+        self.tk_button_clear_tab = self.__tk_button_clear_tab(self)
+        self.tk_button_load_tab = self.__tk_button_load_tab(self)
+        self.tk_label_load_label = self.__tk_label_load_label(self)
+        self.tk_select_box_repeat_if = self.__tk_select_box_repeat_if(self)
+        self.tk_select_box_save_clear_box = self.__tk_select_box_save_clear_box(self)
+        self.tk_select_box_select_method = self.__tk_select_box_select_method(self)
+    def __win(self):
+        self.title("随机数生成器")
+        # 设置窗口大小、居中
+        width = 540
+        height = 400
+        screenwidth = self.winfo_screenwidth()
+        screenheight = self.winfo_screenheight()
+        geometry = '%dx%d+%d+%d' % (width, height, (screenwidth - width) / 2, (screenheight - height) / 2)
+        self.geometry(geometry)
+        
+        self.minsize(width=width, height=height)
+        
+    def scrollbar_autohide(self,vbar, hbar, widget):
+        """自动隐藏滚动条"""
+        def show():
+            if vbar: vbar.lift(widget)
+            if hbar: hbar.lift(widget)
+        def hide():
+            if vbar: vbar.lower(widget)
+            if hbar: hbar.lower(widget)
+        hide()
+        widget.bind("<Enter>", lambda e: show())
+        if vbar: vbar.bind("<Enter>", lambda e: show())
+        if vbar: vbar.bind("<Leave>", lambda e: hide())
+        if hbar: hbar.bind("<Enter>", lambda e: show())
+        if hbar: hbar.bind("<Leave>", lambda e: hide())
+        widget.bind("<Leave>", lambda e: hide())
+    
+    def v_scrollbar(self,vbar, widget, x, y, w, h, pw, ph):
+        widget.configure(yscrollcommand=vbar.set)
+        vbar.config(command=widget.yview)
+        vbar.place(relx=(w + x) / pw, rely=y / ph, relheight=h / ph, anchor='ne')
+    def h_scrollbar(self,hbar, widget, x, y, w, h, pw, ph):
+        widget.configure(xscrollcommand=hbar.set)
+        hbar.config(command=widget.xview)
+        hbar.place(relx=x / pw, rely=(y + h) / ph, relwidth=w / pw, anchor='sw')
+    def create_bar(self,master, widget,is_vbar,is_hbar, x, y, w, h, pw, ph):
+        vbar, hbar = None, None
+        if is_vbar:
+            vbar = Scrollbar(master)
+            self.v_scrollbar(vbar, widget, x, y, w, h, pw, ph)
+        if is_hbar:
+            hbar = Scrollbar(master, orient="horizontal")
+            self.h_scrollbar(hbar, widget, x, y, w, h, pw, ph)
+        self.scrollbar_autohide(vbar, hbar, widget)
+    def new_style(self,widget):
+        ctl = widget.cget('style')
+        ctl = "".join(random.sample('0123456789',5)) + "." + ctl
+        widget.configure(style=ctl)
+        return ctl
+    def __tk_input_start_num(self,parent):
+        ipt = Entry(parent, bootstyle="primary")
+        ipt.insert(0, "1")
+        ipt.place(relx=0.1204, rely=0.0375, relwidth=0.1852, relheight=0.0750)
+        return ipt
+    def __tk_label_from_label(self,parent):
+        label = Label(parent,text="~",anchor="center", bootstyle="primary")
+        label.place(relx=0.3333, rely=0.0375, relwidth=0.0926, relheight=0.0750)
+        return label
+    def __tk_input_end_num(self,parent):
+        ipt = Entry(parent, bootstyle="primary")
+        ipt.insert(0, "10")
+        ipt.place(relx=0.5463, rely=0.0375, relwidth=0.1852, relheight=0.0750)
+        return ipt
+    def __tk_label_now_num(self,parent):
+        label = Label(parent,text="0",anchor="center", bootstyle="info")
+        label.place(relx=0.0259, rely=0.2700, relwidth=0.1852, relheight=0.3625)
+        return label
+    def __tk_button_create_num(self,parent):
+        btn = Button(parent, text="生成", takefocus=False,bootstyle="default")
+        btn.place(relx=0.8148, rely=0.7750, relwidth=0.1481, relheight=0.1875)
+        return btn
+    def __tk_table_num_collect(self,parent):
+        # 表头字段 表头宽度
+        columns = {"Index":77,"Content":311}
+        tk_table = Treeview(parent, show="headings", columns=list(columns),bootstyle="primary")
+        for text, width in columns.items():  # 批量设置列属性
+            tk_table.heading(text, text=text, anchor='center')
+            tk_table.column(text, anchor='center', width=width, stretch=True)  # stretch 不自动拉伸
+        
+        tk_table.place(relx=0.2407, rely=0.1750, relwidth=0.7222, relheight=0.5500)
+        self.create_bar(parent, tk_table,True, False,130, 70, 390,220,540,400)
+        return tk_table
+    def __tk_label_start_label(self,parent):
+        label = Label(parent,text="最小:",anchor="center", bootstyle="primary")
+        label.place(relx=0.0278, rely=0.0375, relwidth=0.0926, relheight=0.0750)
+        return label
+    def __tk_label_end_label(self,parent):
+        label = Label(parent,text="最大:",anchor="center", bootstyle="primary")
+        label.place(relx=0.4537, rely=0.0375, relwidth=0.0926, relheight=0.0750)
+        return label
+    def __tk_input_generation_num(self,parent):
+        ipt = Entry(parent, bootstyle="default")
+        ipt.place(relx=0.1204, rely=0.7750, relwidth=0.1852, relheight=0.0750)
+        ipt.insert(0, "1")
+        return ipt
+    def __tk_label_generation_label(self,parent):
+        label = Label(parent,text="个数:",anchor="center", bootstyle="primary")
+        label.place(relx=0.0278, rely=0.7750, relwidth=0.0926, relheight=0.0750)
+        return label
+    def __tk_button_clear_tab(self,parent):
+        btn = Button(parent, text="清除", takefocus=False,bootstyle="info")
+        btn.place(relx=0.6167, rely=0.7750, relwidth=0.1778, relheight=0.0750)
+        return btn
+    def __tk_button_load_tab(self,parent):
+        btn = Button(parent, text="读取Excel生成", takefocus=False,bootstyle="default")
+        btn.place(relx=0.3241, rely=0.7750, relwidth=0.2778, relheight=0.0750)
+        return btn
+    def __tk_label_load_label(self,parent):
+        label = Label(parent,text="目前读取内容：空",anchor="center", bootstyle="secondary")
+        label.place(relx=0.0296, rely=0.8875, relwidth=0.7593, relheight=0.0750)
+        return label
+    def __tk_select_box_repeat_if(self,parent):
+        cb = Combobox(parent, state="readonly", bootstyle="info")
+        cb['values'] = ("不重复","重复")
+        cb.set("不重复")
+        cb.place(relx=0.7593, rely=0.0375, relwidth=0.2037, relheight=0.0750)
+        return cb
+    def __tk_select_box_save_clear_box(self,parent):
+        cb = Combobox(parent, state="readonly", bootstyle="primary")
+        cb['values'] = ("自动清除","保留")
+        cb.set("自动清除")
+        cb.place(relx=0.0259, rely=0.6550, relwidth=0.1852, relheight=0.0750)
+        return cb
+    def __tk_select_box_select_method(self,parent):
+        cb = Combobox(parent, state="readonly", bootstyle="primary")
+        cb['values'] = ("数字")
+        cb.set("数字")
+        cb.place(relx=0.0259, rely=0.1750, relwidth=0.1852, relheight=0.0750)
+        return cb
+class Win(WinGUI):
+    def __init__(self, controller):
+        self.ctl = controller
+        super().__init__()
+        self.__event_bind()
+        self.__style_config()
+        self.config(menu=self.create_menu())
+        self.ctl.init(self)
+    def create_menu(self):
+        menu = Menu(self,tearoff=False)
+        menu.add_command(label="导出结果",command=self.ctl.save_txt)
+        return menu
+    def __event_bind(self):
+        self.tk_button_create_num.bind('<Button-1>', lambda event: self.ctl.create_number( 
+        int(self.tk_input_start_num.get()),          # 最小值
+        int(self.tk_input_end_num.get()),            # 最大值
+        int(self.tk_input_generation_num.get()),     # 生成数量
+        self.tk_select_box_repeat_if.get(),          # 是否重复
+        self.tk_select_box_select_method.get(),      # 生成方法
+        self.tk_select_box_save_clear_box.get() ))   # 是否自动清除
+        self.tk_button_clear_tab.bind('<Button-1>',self.ctl.clear_table)
+        self.tk_button_load_tab.bind('<Button-1>',self.ctl.load_execl)
+        pass
+    def __style_config(self):
+        sty = Style()
+        sty.configure(self.new_style(self.tk_label_from_label),font=("微软雅黑",-22,"bold"))
+        sty.configure(self.new_style(self.tk_label_now_num),font=("微软雅黑",-25,"bold underline"))
+        sty.configure(self.new_style(self.tk_button_create_num),font=("微软雅黑",-20,"bold"))
+        sty.configure(self.new_style(self.tk_label_start_label),font=("微软雅黑",-18,"bold"))
+        sty.configure(self.new_style(self.tk_label_end_label),font=("微软雅黑",-18,"bold"))
+        sty.configure(self.new_style(self.tk_label_generation_label),font=("微软雅黑",-18,"bold"))
+        sty.configure(self.new_style(self.tk_button_clear_tab),font=("微软雅黑",-15,"bold"))
+        sty.configure(self.new_style(self.tk_button_load_tab),font=("微软雅黑",-15,"bold"))
+        sty.configure(self.new_style(self.tk_label_load_label),font=("微软雅黑",-13,"underline"))
+        sty.configure("Treeview", font=("微软雅黑", 11))  # 单元格字体大小
+        pass
+if __name__ == "__main__":
+    win = WinGUI()
+    win.mainloop()
