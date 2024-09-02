f06e0232444920edbb3b8a9fceebfd6a850026c1
mrmanforgithub <3141356420@qq.com>
2024-09-02 21:20:34 +0800

    可以设置浮点数，设置密码详情

--- control.py
+++ control.py
@@ -5,6 +5,8 @@ import string
 from openpyxl import load_workbook
 import xlrd
 import re
+from decimal import Decimal, getcontext
+from tkinter import messagebox
 
 class Controller:
     ui: object
@@ -37,16 +39,237 @@ class Controller:
     def limit_number(self,value, max_value):
         return min(value, max_value)
 
-    def get_characters_range(self,method, start_num, end_num):
-        if method == "小写字母":
-            characters = string.ascii_lowercase
-        elif method == "大写字母":
-            characters = string.ascii_uppercase
+    def generate_numbers_from_range(self, start_num, end_num, generation_num, repeat_if,float_precision,load_num=None, ):
+        if float_precision != 0:
+            return self._generate_floats(start_num, end_num, generation_num, repeat_if, float_precision)
         else:
-            return None
-        if end_num > 26:
-            end_num = 26
+            return self._generate_integers(start_num, end_num, generation_num, repeat_if, load_num)
+
+    def _generate_floats(self, start_num, end_num, generation_num, repeat_if, float_precision):
+        # 设置 Decimal 的精度
+        getcontext().prec = float_precision + 5  # 增加额外的精度以防计算误差
+
+        def generate_float():
+            # 使用 Decimal 生成精确的浮点数
+            return round(Decimal(random.uniform(start_num, end_num)), float_precision)
+
+        if repeat_if == "重复":
+            return [generate_float() for _ in range(generation_num)]
+
+        # 对于不重复的浮点数生成
+        # 计算可能的浮点数范围
+        step = Decimal(1) / (10 ** float_precision)
+        num_steps = int((Decimal(end_num) - Decimal(start_num)) / step) + 1
+
+        if generation_num > num_steps:
+            generation_num = num_steps
+
+        # 生成不重复的浮点数
+        float_values = set()
+        while len(float_values) < generation_num:
+            value = generate_float()
+            if Decimal(start_num) <= value <= Decimal(end_num):
+                float_values.add(value)
+
+        return list(float_values)
+
+    def _generate_integers(self, start_num, end_num, generation_num, repeat_if, load_num):
+        def handle_repeating_logic(start, end, num, repeat_mode, max_num=None):
+            if max_num is not None:
+                if max_num < start:
+                    return []
+                end = min(end, max_num)
+            if repeat_mode == "重复":
+                return [random.randint(start, end) for _ in range(num)]
+            elif repeat_mode == "不重复":
+                available_range_size = end - start + 1
+                if num > available_range_size:
+                    num = available_range_size
+                return random.sample(range(start, end + 1), num)
+
+        if load_num is not None:
+            return handle_repeating_logic(start_num, end_num, generation_num, repeat_if, load_num)
+
+        return handle_repeating_logic(start_num, end_num, generation_num, repeat_if)
+
+
+
+    def validate_password_strength(self, password):
+        # 使用正则表达式检查密码是否包含大写字母、小写字母、数字和特殊字符
+        has_upper = bool(re.search(r'[A-Z]', password))
+        has_lower = bool(re.search(r'[a-z]', password))
+        has_digit = bool(re.search(r'\d', password))
+        has_special = bool(re.search(r'[!@#%^&*(),.?":{}|<>]', password))
+
+        # 根据符合的条件数量，判断密码强度
+        strength = sum([has_upper, has_lower, has_digit, has_special])
+
+        if strength == 4:
+            return "强密码"
+        elif strength == 3:
+            return "中密码"
+        else:
+            return "低密码"
+
+    def create_number(self, start_num, end_num, generation_num, repeat_if, method, clear_if, special_setting):
+        if method == "密码":
+            remove_least = special_setting.get("password", {}).get("remove_least", False)
+            if remove_least:
+                start_num = max(0, start_num)
+                end_num = max(0, end_num, start_num)
+            else:
+                start_num = max(10, start_num)
+                end_num = max(10, end_num, start_num)
+            self.ui.tk_input_start_num.delete(0, "end")
+            self.ui.tk_input_start_num.insert(0, start_num)
+            self.ui.tk_input_end_num.delete(0, "end")
+            self.ui.tk_input_end_num.insert(0, end_num)
+            generated_numbers = self.generate_random_passwords(start_num, end_num, generation_num,special_setting.get("password", {
+                "include_numbers": True,
+                "include_lower": True,
+                "include_character": True,
+                "include_special_chars": True,
+                "exclude_similar_chars": False}))
+        else:
+            if start_num > end_num:
+                start_num, end_num = end_num, start_num
+            if method == "字母":
+                letter_case = special_setting.get("letter", {"letter_case": "lower"}).get("letter_case", "lower")
+                characters_range = self.get_characters_range(letter_case, start_num, end_num)
+                generated_numbers = self.generate_numbers_from_characters(generation_num, repeat_if, characters_range)
+            elif method not in ["数字", "字母", "密码"] and self.load_if:
+                start_num = max(1, start_num)
+                end_num = max(1, end_num)
+                names_from_excel = self.handle_excel_data(method, end_num, self.load_if)
+                generated_indices = self.generate_numbers_from_range(start_num, end_num, generation_num, repeat_if,0, load_num=len(names_from_excel))
+                generated_numbers = [names_from_excel[i-1] for i in generated_indices]
+            else:
+                generated_numbers = self.generate_numbers_from_range(start_num, end_num, generation_num, repeat_if,float_precision=special_setting.get("number", {"float_precision": 0}).get("float_precision", 0))
+
+        if clear_if == "自动清除":
+            self.ui.tk_table_num_collect.delete(*self.ui.tk_table_num_collect.get_children())
+
+        for i, num in enumerate(generated_numbers, start=1):
+            display_value = num
+            if method == "密码":
+                password_strength = self.validate_password_strength(num)
+                self.ui.tk_label_now_num.config(text=password_strength)
+            else:
+                self.ui.tk_label_now_num.config(text=display_value)
+            self.ui.update()
+            time.sleep(0.02)
+            self.ui.tk_table_num_collect.insert('', 'end', values=(i, display_value))
+
+    def generate_random_passwords(self, min_length, max_length, generation_num, settings):
+        include_numbers = settings.get("include_numbers", True)
+        include_lowers = settings.get("include_lower", True)
+        include_uppers = settings.get("include_character", True)
+        include_special_chars = settings.get("include_special_chars", True)
+        exclude_similar_chars = settings.get("exclude_similar_chars", False)
+
+        # 初始化字符集
+        lowers = string.ascii_lowercase if include_lowers else ""
+        uppers = string.ascii_uppercase if include_uppers else ""
+        numbers = string.digits if include_numbers else ""
+        special_chars = "!@#%^&*" if include_special_chars else ""
+        characters = lowers + uppers + numbers + special_chars
+
+        # 处理排除相似字符的设置
+        if exclude_similar_chars:
+            similar_chars = 'il1Lo0O'
+            characters = ''.join(c for c in characters if c not in similar_chars)
+
+        if not characters:
+            messagebox.showerror("错误", "字符集为空，请检查设置")
+            return []
+
+        passwords = []
+
+        for _ in range(generation_num):
+            while True:
+                password_length = random.randint(min_length, max_length)
+                password = ''.join(random.choice(characters) for _ in range(password_length))
+
+                if exclude_similar_chars:# 检查是否包含至少一个大写字母、小写字母、数字和特殊符号
+                    if (not include_uppers or any(c in uppers for c in password)) and \
+                    (not include_lowers or any(c in lowers for c in password)) and \
+                    (not include_numbers or any(c in numbers for c in password)) and \
+                    (not include_special_chars or any(c in special_chars for c in password)):
+
+                        def has_consecutive_sequence(s):
+                            for i in range(len(s) - 2):
+                                if ord(s[i+1]) == ord(s[i]) + 1 == ord(s[i+2]) - 1:
+                                    return True
+                            return False
+
+                        if not has_consecutive_sequence(password):
+                            passwords.append(password)
+                            break
+                else:
+                    passwords.append(password)
+                    break
+
+        return passwords
+
+    def get_characters_range(self, letter_case, start_num, end_num):
+        if letter_case == "lower":
+            characters = string.ascii_lowercase
+        elif letter_case == "upper":
+            characters = string.ascii_uppercase
+        elif letter_case == "mixed":
+            characters = string.ascii_letters  # 组合大小写字母
+        else:
+            return 'X'
+        # 确保 end_num 不超过字符集长度
+        if end_num > len(characters):
+            end_num = len(characters)
+        # 处理 start_num 可能小于 1 的情况
+        if start_num < 1:
+            start_num = 1
         return characters[start_num-1:end_num]
+
+    def generate_numbers_from_characters(self, generation_num, repeat_if, characters_range):
+        available_range_size = len(characters_range)
+        if repeat_if == "不重复" and generation_num > available_range_size:
+            generation_num = available_range_size
+        if repeat_if == "重复":
+            return [random.choice(characters_range) for _ in range(generation_num)]
+        elif repeat_if == "不重复":
+            return random.sample(characters_range, generation_num)
+
+    def clear_table(self,evt):
+        self.ui.tk_table_num_collect.delete(*self.ui.tk_table_num_collect.get_children())
+
+    def load_execl(self, evt):
+        if not self.load_if:
+            file_path = filedialog.askopenfilename(filetypes=[("Excel files", "*.xlsx;*.xls")])
+            if file_path:
+                self.load_if = file_path
+                self.ui.tk_label_load_label.config(text=file_path)
+
+                # 根据文件扩展名决定使用哪个库
+                if file_path.endswith('.xlsx'):
+                    wb = load_workbook(file_path)
+                    sheet = wb.active  # 获取活动工作表
+                    column_count = sheet.max_column
+                elif file_path.endswith('.xls'):
+                    wb = xlrd.open_workbook(file_path)
+                    sheet = wb.sheet_by_index(0)  # 获取第一个工作表
+                    column_count = sheet.ncols  # 获取列数
+                else:
+                    self.ui.tk_label_load_label.config(text="不支持的文件格式")
+                    return
+
+                cb = self.ui.tk_select_box_select_method  # 调用创建Combobox的方法
+                column_options = ["数字", "字母","密码"] + [f"第{i}列" for i in range(1, column_count + 1)]
+                cb['values'] = column_options  # 设置Combobox的选项为列数的范围
+                cb.set("第1列")  # 默认选择第一列
+        else:
+            self.load_if = None
+            self.ui.tk_label_load_label.config(text="目前读取内容：空")
+            cb = self.ui.tk_select_box_select_method
+            cb['values'] = ["数字","字母","密码"]
+            cb.set("数字")
 
     def handle_excel_data(self, method, end_num, load_if):
         if load_if.endswith('.xlsx'):
@@ -74,140 +297,3 @@ class Controller:
             raise ValueError("Unsupported file format")
 
         return names_from_excel
-
-    def generate_numbers_from_characters(self,start_num, end_num, generation_num, repeat_if, characters_range):
-        available_range_size = len(characters_range)
-        if repeat_if == "不重复" and generation_num > available_range_size:
-            generation_num = available_range_size
-        if repeat_if == "重复":
-            return [random.choice(characters_range) for _ in range(generation_num)]
-        elif repeat_if == "不重复":
-            return random.sample(characters_range, generation_num)
-
-    def generate_numbers_from_range(self,start_num, end_num, generation_num, repeat_if, load_num=None):
-        if load_num is not None:
-            if load_num < start_num:
-                return []
-            if repeat_if == "重复":
-                if load_num < end_num:
-                    end_num = load_num
-                return [random.randint(start_num, end_num) for _ in range(generation_num)]
-            elif repeat_if == "不重复":
-                available_range_size = end_num - start_num + 1
-                if generation_num > available_range_size:
-                    generation_num = available_range_size
-                if load_num < generation_num:
-                    generation_num = load_num
-                if load_num < end_num:
-                    end_num = load_num
-                return random.sample(range(start_num, end_num + 1), generation_num)
-        else:
-            if repeat_if == "重复":
-                return [random.randint(start_num, end_num) for _ in range(generation_num)]
-            elif repeat_if == "不重复":
-                available_range_size = end_num - start_num + 1
-                if generation_num > available_range_size:
-                    generation_num = available_range_size
-                return random.sample(range(start_num, end_num + 1), generation_num)
-
-    def validate_password_strength(self, password):
-        # 使用正则表达式检查密码是否包含大写字母、小写字母、数字和特殊字符
-        has_upper = bool(re.search(r'[A-Z]', password))
-        has_lower = bool(re.search(r'[a-z]', password))
-        has_digit = bool(re.search(r'\d', password))
-        has_special = bool(re.search(r'[!@#%^&*(),.?":{}|<>]', password))
-
-        # 根据符合的条件数量，判断密码强度
-        strength = sum([has_upper, has_lower, has_digit, has_special])
-
-        if strength == 4:
-            return "强密码"
-        elif strength == 3:
-            return "中密码"
-        else:
-            return "低密码"
-
-    def create_number(self, start_num, end_num, generation_num, repeat_if, method, clear_if):
-        if method == "密码":
-            start_num = max(10, start_num)
-            end_num = max(10, end_num, start_num)
-            self.ui.tk_input_start_num.delete(0, "end")
-            self.ui.tk_input_start_num.insert(0, start_num)
-            self.ui.tk_input_end_num.delete(0, "end")
-            self.ui.tk_input_end_num.insert(0, end_num)
-            generated_numbers = self.generate_random_passwords(start_num, end_num, generation_num)
-        else:
-            if start_num > end_num:
-                start_num, end_num = end_num, start_num
-
-            if method in ["小写字母", "大写字母"]:
-                characters_range = self.get_characters_range(method, start_num, end_num)
-                generated_numbers = self.generate_numbers_from_characters(start_num, end_num, generation_num, repeat_if, characters_range)
-            elif method not in ["数字", "大写字母", "小写字母", "密码"] and self.load_if:
-                start_num = max(1, start_num)
-                end_num = max(1, end_num)
-                names_from_excel = self.handle_excel_data(method, end_num, self.load_if)
-                generated_indices = self.generate_numbers_from_range(start_num, end_num, generation_num, repeat_if, load_num=len(names_from_excel))
-                generated_numbers = [names_from_excel[i-1] for i in generated_indices]
-            else:
-                generated_numbers = self.generate_numbers_from_range(start_num, end_num, generation_num, repeat_if)
-
-        if clear_if == "自动清除":
-            self.ui.tk_table_num_collect.delete(*self.ui.tk_table_num_collect.get_children())
-
-        for i, num in enumerate(generated_numbers, start=1):
-            display_value = num
-            if method == "密码":
-                password_strength = self.validate_password_strength(num)
-                self.ui.tk_label_now_num.config(text=password_strength)
-            else:
-                self.ui.tk_label_now_num.config(text=display_value)
-            self.ui.update()
-            time.sleep(0.02)
-            self.ui.tk_table_num_collect.insert('', 'end', values=(i, display_value))
-
-    def generate_random_passwords(self, min_length, max_length, generation_num):
-        characters = string.ascii_letters + string.digits + "!@#%^&*"
-        passwords = []
-
-        for _ in range(generation_num):
-            password_length = random.randint(min_length, max_length)
-            password = ''.join(random.choice(characters) for _ in range(password_length))
-            passwords.append(password)
-
-        return passwords
-
-
-    def clear_table(self,evt):
-        self.ui.tk_table_num_collect.delete(*self.ui.tk_table_num_collect.get_children())
-
-    def load_execl(self, evt):
-        if not self.load_if:
-            file_path = filedialog.askopenfilename(filetypes=[("Excel files", "*.xlsx;*.xls")])
-            if file_path:
-                self.load_if = file_path
-                self.ui.tk_label_load_label.config(text=file_path)
-
-                # 根据文件扩展名决定使用哪个库
-                if file_path.endswith('.xlsx'):
-                    wb = load_workbook(file_path)
-                    sheet = wb.active  # 获取活动工作表
-                    column_count = sheet.max_column
-                elif file_path.endswith('.xls'):
-                    wb = xlrd.open_workbook(file_path)
-                    sheet = wb.sheet_by_index(0)  # 获取第一个工作表
-                    column_count = sheet.ncols  # 获取列数
-                else:
-                    self.ui.tk_label_load_label.config(text="不支持的文件格式")
-                    return
-
-                cb = self.ui.tk_select_box_select_method  # 调用创建Combobox的方法
-                column_options = ["数字", "小写字母", "大写字母","密码"] + [f"第{i}列" for i in range(1, column_count + 1)]
-                cb['values'] = column_options  # 设置Combobox的选项为列数的范围
-                cb.set("第1列")  # 默认选择第一列
-        else:
-            self.load_if = None
-            self.ui.tk_label_load_label.config(text="目前读取内容：空")
-            cb = self.ui.tk_select_box_select_method
-            cb['values'] = ["数字", "小写字母", "大写字母","密码"]
-            cb.set("数字")
--- ui.py
+++ ui.py
@@ -4,6 +4,8 @@ from tkinter.ttk import *
 from ttkbootstrap import *
 from pytkUI.widgets import *
 from tkinter import messagebox
+import json
+
 class WinGUI(Window):
     def __init__(self):
         super().__init__(themename="cosmo", hdpi=False)
@@ -103,7 +105,6 @@ class WinGUI(Window):
         for text, width in columns.items():  # 批量设置列属性
             tk_table.heading(text, text=text, anchor='center')
             tk_table.column(text, anchor='center', width=width, stretch=True)  # stretch 不自动拉伸
-
         tk_table.place(relx=0.2407, rely=0.1750, relwidth=0.7222, relheight=0.5500)
         self.create_bar(parent, tk_table,True, False,130, 70, 390,220,540,400)
         return tk_table
@@ -150,22 +151,127 @@ class WinGUI(Window):
         return cb
     def __tk_select_box_select_method(self,parent):
         cb = Combobox(parent, state="readonly", bootstyle="primary")
-        cb['values'] = ("数字","小写字母","大写字母","密码")
+        cb['values'] = ("数字","字母","密码")
         cb.set("数字")
         cb.place(relx=0.0259, rely=0.1750, relwidth=0.1852, relheight=0.0750)
         return cb
 class Win(WinGUI):
     def __init__(self, controller):
         self.ctl = controller
+        self.current_settings = {
+            "number": {
+                "float_precision": 0
+            },
+            "letter": {
+                "letter_case": "lower"
+            },
+            "password": {
+                "include_numbers": True,
+                "include_lower": True,
+                "include_character": True,
+                "include_special_chars": True,
+                "exclude_similar_chars": False,
+                "remove_least": False
+            }
+        }
         super().__init__()
         self.__event_bind()
         self.__style_config()
         self.config(menu=self.create_menu())
         self.ctl.init(self)
+
     def create_menu(self):
-        menu = Menu(self,tearoff=False)
-        menu.add_command(label="导出结果",command=self.ctl.save_txt)
-        return menu
+        menu_bar = Menu(self)
+        # 创建“设置”菜单
+        settings_menu = Menu(menu_bar, tearoff=0)
+        settings_menu.add_command(label="导出到TXT", command=self.ctl.save_txt)
+        settings_menu.add_command(label="当前模式设置", command=self.show_settings)
+        menu_bar.add_cascade(label="菜单", menu=settings_menu)
+
+        return menu_bar
+
+    def show_settings(self):
+        selected_method = self.tk_select_box_select_method.get()
+        if selected_method == '数字':
+            self.show_number_settings()
+        elif selected_method == '字母':
+            self.show_letter_settings()
+        elif selected_method == '密码':
+            self.show_password_settings()
+
+    def show_number_settings(self):
+        # 数字模式的设置窗口
+        settings_window = Toplevel(self)
+        settings_window.title("数字设置")
+        settings_window.geometry("250x120")
+
+        Label(settings_window, text="浮点数位数:").place(x=20, y=20, width=80, height=25)
+        float_precision = Entry(settings_window)
+        float_precision.insert(0, self.current_settings.get("number", {}).get("float_precision", 0))
+        float_precision.place(x=110, y=20, width=100, height=25)
+
+        Button(settings_window, text="保存", command=lambda: self.save_settings(
+            {"mode": "number", "float_precision": max(int(float_precision.get()),0)},
+        settings_window
+        )).place(x=75, y=70, width=100, height=30)
+
+
+    def show_letter_settings(self):
+        # 字母模式的设置窗口
+        settings_window = Toplevel(self)
+        settings_window.title("字母设置")
+        settings_window.geometry("300x120")
+
+        letter_case = StringVar(value=self.current_settings.get("letter", {}).get("letter_case", "lower"))
+        Radiobutton(settings_window, text="小写字母", variable=letter_case, value="lower").place(x=20, y=20, width=80, height=25)
+        Radiobutton(settings_window, text="大写字母", variable=letter_case, value="upper").place(x=110, y=20, width=80, height=25)
+        Radiobutton(settings_window, text="大小写混合", variable=letter_case, value="mixed").place(x=200, y=20, width=80, height=25)
+
+        Button(settings_window, text="保存", command=lambda: self.save_settings(
+            {"mode": "letter", "letter_case": letter_case.get()},
+        settings_window
+        )).place(x=100, y=70, width=100, height=30)
+
+    def show_password_settings(self):
+        # 密码模式的设置窗口
+        settings_window = Toplevel(self)
+        settings_window.title("密码设置")
+        settings_window.geometry("280x300")
+
+        include_numbers = BooleanVar(value=self.current_settings.get("password", {}).get("include_numbers", "True"))
+        include_lower = BooleanVar(value=self.current_settings.get("password", {}).get("include_lower", "True"))
+        include_character = BooleanVar(value=self.current_settings.get("password", {}).get("include_character", "True"))
+        include_special_chars = BooleanVar(value=self.current_settings.get("password", {}).get("include_special_chars", "True"))
+        exclude_similar_chars = BooleanVar(value=self.current_settings.get("password", {}).get("exclude_similar_chars", "False"))
+        remove_least = BooleanVar(value=self.current_settings.get("password", {}).get("remove_least", "False"))
+
+        # 使用 place 方法定位组件
+        Checkbutton(settings_window, text="包含数字", variable=include_numbers).place(x=20, y=20, width=120, height=25)
+        Checkbutton(settings_window, text="包含小写字母", variable=include_lower).place(x=20, y=60, width=120, height=25)
+        Checkbutton(settings_window, text="包含大写字母", variable=include_character).place(x=20, y=100, width=120, height=25)
+        Checkbutton(settings_window, text="包含特殊符号", variable=include_special_chars).place(x=20, y=140, width=120, height=25)
+        Checkbutton(settings_window, text="提高密码强度", variable=exclude_similar_chars).place(x=20, y=180, width=120, height=25)
+        Checkbutton(settings_window, text="去除最小密码长度", variable=remove_least).place(x=20, y=220, width=120, height=25)
+
+        Button(settings_window, text="保存", command=lambda: self.save_settings(
+            {"mode": "password",
+            "include_numbers": include_numbers.get(),
+            "include_lower": include_lower.get(),
+            "include_character": include_character.get(),
+            "include_special_chars": include_special_chars.get(),
+            "exclude_similar_chars": exclude_similar_chars.get(),
+            "remove_least": remove_least.get(),},
+        settings_window
+        )).place(x=100, y=260, width=100, height=30)
+
+    def save_settings(self, settings, window):
+        # 将设置保存为 JSON 字符串
+        mode = settings.get("mode")
+        if mode:
+            self.current_settings[mode] = settings
+            del self.current_settings[mode]["mode"]
+        window.destroy()
+
     def convert_letter_to_number(self,value):
     # 如果输入的是单个字母（忽略大小写），将其转换为对应的数字
         if isinstance(value, str) and len(value) == 1 and value.isalpha():
@@ -178,7 +284,9 @@ class Win(WinGUI):
         int(self.tk_input_generation_num.get()),     # 生成数量
         self.tk_select_box_repeat_if.get(),          # 是否重复
         self.tk_select_box_select_method.get(),      # 生成方法
-        self.tk_select_box_save_clear_box.get() ))   # 是否自动清除
+        self.tk_select_box_save_clear_box.get(),
+        self.current_settings))   # 是否自动清除
+
         self.tk_button_clear_tab.bind('<Button-1>',self.ctl.clear_table)
         self.tk_button_load_tab.bind('<Button-1>',self.ctl.load_execl)
         self.tk_table_num_collect.bind("<Button-3>", self.show_context_menu)  # 右键点击
